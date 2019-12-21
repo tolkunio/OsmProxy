c40c723e8db0e4dbf2609408894958523927635b
Tolkun Omurbekova <tolkun3922@gmail.com>
2019-12-21 21:20:41 +0600

    -refactor: add service layer, extract controller logic to the service

--- WebApi/Controllers/OSMProxyController.cs
+++ WebApi/Controllers/OSMProxyController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
@@ -7,6 +8,7 @@ using MongoDB.Bson;
 using Nominatim.API.Geocoders;
 using Nominatim.API.Models;
 using WebApi.Models;
+using WebApi.Services;
 
 namespace WebApi.Controllers
 {
@@ -15,58 +17,18 @@ namespace WebApi.Controllers
     public class OSMProxyController : ControllerBase
     {
         private readonly ILogger<OSMProxyController> _logger;
-        private readonly ICachedResponseManager _cacheManager;
+        private readonly IOsmProxyService _proxyService;
 
-        public OSMProxyController(ILogger<OSMProxyController> logger, ICachedResponseManager cacheManager)
+        public OSMProxyController(ILogger<OSMProxyController> logger, IOsmProxyService proxyService)
         {
             _logger = logger;
-            _cacheManager = cacheManager;
+            _proxyService = proxyService;
         }
 
         [HttpGet]
-        public async Task<GeocodeResponse[]> Search(string searchString)
+        public async Task<GeocodeResponse[]> Search([Required] string searchString)
         {
-            string searchRequest = searchString.ToLower();
-            _logger.LogInformation("");
-
-            if (string.IsNullOrEmpty(searchString))
-                return null;
-
-            var cacheResponse = _cacheManager.Get(searchRequest);
-           
-            if (cacheResponse != null)
-            {
-                if (_cacheManager.IsActualCache(cacheResponse))
-                {
-                    return await Task.FromResult(cacheResponse.Content);
-                }
-                var actualCache = new CachedResponse
-                {
-                    Id = cacheResponse.Id,
-                    SearchText = cacheResponse.SearchText,
-                    CreatedAt = DateTime.Now,
-                    Content = await GetGeocodeResponse(searchRequest)
-                };
-                _cacheManager.UpdateCachedResponse(cacheResponse.Id, actualCache);
-                return actualCache.Content;
-            }
-
-            var geocodeArray = await GetGeocodeResponse(searchString);
-            _cacheManager.CacheResponse(geocodeArray, searchString);
-
-            return geocodeArray;
-        }
-
-        public async Task<GeocodeResponse[]> GetGeocodeResponse(string searchString)
-        {
-            return await new ForwardGeocoder().Geocode(new ForwardGeocodeRequest
-            {
-                queryString = searchString,
-                BreakdownAddressElements = true,
-                ShowExtraTags = true,
-                ShowAlternativeNames = true,
-                ShowGeoJSON = true
-            });
+            return await _proxyService.Search(searchString);
         }
     }
 }
---
+++ WebApi/Services/OsmProxyService.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Nominatim.API.Geocoders;
+using Nominatim.API.Models;
+using WebApi.Models;
+
+namespace WebApi.Services
+{
+    public class OsmProxyService : IOsmProxyService
+    {
+        private readonly ICachedResponseManager _cacheManager;
+
+        public OsmProxyService(ICachedResponseManager cacheManager)
+        {
+            _cacheManager = cacheManager;
+        }
+
+        public async Task<GeocodeResponse[]> Search(string searchText)
+        {
+            var normalizedSearchText = searchText.ToLower();
+
+            var cacheResponse = _cacheManager.Get(searchText);
+
+            if (cacheResponse == null)
+            {
+                var response = await RemoteSearch(normalizedSearchText);
+                _cacheManager.CacheResponse(response, normalizedSearchText);
+                return response;
+            }
+
+            if (_cacheManager.IsActualCache(cacheResponse))
+            {
+                return await Task.FromResult(cacheResponse.Content);
+            }
+
+            var geocodeResponses = await RemoteSearch(normalizedSearchText);
+
+            var actualCache = new CachedResponse
+            {
+                Id = cacheResponse.Id,
+                SearchText = cacheResponse.SearchText,
+                CreatedAt = DateTime.Now,
+                Content = geocodeResponses
+            };
+
+            _cacheManager.UpdateCachedResponse(cacheResponse.Id, actualCache);
+            return geocodeResponses;
+        }
+
+        private static async Task<GeocodeResponse[]> RemoteSearch(string normalizedSearchText)
+        {
+            var forwardGeocoder = new ForwardGeocoder();
+
+            return await forwardGeocoder.Geocode(new ForwardGeocodeRequest
+            {
+                queryString = normalizedSearchText,
+                BreakdownAddressElements = true,
+                ShowExtraTags = true,
+                ShowAlternativeNames = true,
+                ShowGeoJSON = true
+            });
+        }
+    }
+}
